8d2d04ddca6e281fd03b4fdb00397966304c0423
almanurfajriah18 <115517620+almanurfajriah18@users.noreply.github.com>
2023-07-12 19:24:10 +0700

    footer

--- resources/views/user/footer.blade.php
+++ resources/views/user/footer.blade.php
@@ -3,20 +3,17 @@
   <footer class="py-5">
     <div class="row">
       <div class="col-4">
-        <h5>Tentang kita</h5>
-        <ul class="nav flex-column">
-          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Website ini dibuat untuk membantu mahasiswa mengatasi berbagai permasalahan terkait kegiatan/layanan akademik selama menempuh studi, Kemahasiswaan membuka Layanan Pengaduan bagi Mahasiswa.</a></li>
-        </ul>
+      <img src="{{ asset('images/Logo.svg') }}" alt="Logo"  height="30" class="d-inline-block align-text-top">
       </div>
 
       <div class="col-4">
         <h5>Link Penting</h5>
         <ul class="nav flex-column">
-          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Beranda</a></li>
-          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Pengaduan</a></li>
-          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Prosedur</a></li>
-          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Laporan</a></li>
-          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Kontak</a></li>
+          <li class="nav-item mb-2"><a href="{{ route('depan.home') }}" class="nav-link p-0 text-muted">Beranda</a></li>
+          <li class="nav-item mb-2"><a href="{{ route('depan.pengaduan') }}" class="nav-link p-0 text-muted">Pengaduan</a></li>
+          <li class="nav-item mb-2"><a href="{{ route('depan.prosedur') }}" class="nav-link p-0 text-muted">Prosedur</a></li>
+          <li class="nav-item mb-2"><a href="{{ route('depan.laporan') }}" class="nav-link p-0 text-muted">Laporan</a></li>
+          <li class="nav-item mb-2"><a href="{{ route('depan.kontak') }}" class="nav-link p-0 text-muted">Kontak</a></li>
         </ul>
       </div>
 
@@ -32,12 +29,12 @@
       </div>
     </div>
 
-    <div class="d-flex justify-content-between py-4 my-4 border-top">
-      <p>&copy; 2021 Company, Inc. All rights reserved.</p>
+    <div class="text-center py-4 my-4 border-top">
+      <p text-center>&copy; 2023 Copyright by Mahasiswa STT Terpadu Nurul Fikri. All rights reserved.</p>
       <ul class="list-unstyled d-flex">
-        <li class="ms-3"><a class="link-dark" href="#"><svg class="bi" width="24" height="24"><use xlink:href="#twitter"/></svg></a></li>
-        <li class="ms-3"><a class="link-dark" href="#"><svg class="bi" width="24" height="24"><use xlink:href="#instagram"/></svg></a></li>
-        <li class="ms-3"><a class="link-dark" href="#"><svg class="bi" width="24" height="24"><use xlink:href="#facebook"/></svg></a></li>
+        <li class="ms-3"><a class="link-dark" href="https://twitter.com/sttnurulfikri"><svg class="bi" width="24" height="24"><use xlink:href="#twitter"/></svg></a></li>
+        <li class="ms-3"><a class="link-dark" href="https://www.instagram.com/sttterpadunf/?hl=id"><svg class="bi" width="24" height="24"><use xlink:href="#instagram"/></svg></a></li>
+        <li class="ms-3"><a class="link-dark" href="https://www.facebook.com/STTTerpaduNF/?locale=id_ID"><svg class="bi" width="24" height="24"><use xlink:href="#facebook"/></svg></a></li>
       </ul>
     </div>
   </footer>
